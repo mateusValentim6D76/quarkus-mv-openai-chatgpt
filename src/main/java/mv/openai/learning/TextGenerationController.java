c9b3a337cccd116e7049ab9c05e004b2497f8de4
mateusValentim <mateus.valentim19@hotmail.com>
2023-08-22 21:35:02 -0300

    Finalizing the text controller

--- src/main/java/mv/openai/learning/TextGenerationController.java
+++ src/main/java/mv/openai/learning/TextGenerationController.java
@@ -1,6 +1,11 @@
 package mv.openai.learning;
 
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -9,4 +14,14 @@ import javax.ws.rs.core.MediaType;
 @Path("text.html")
 @Produces(MediaType.TEXT_HTML)
 public class TextGenerationController {
+
+    @Location("text.html")
+    private Template template;
+    @Inject
+    private OpenAIClient openAIClient;
+
+    public TemplateInstance text(){
+        String text = openAIClient.generateText("Generate a snippet of some Djavan songs");
+        return template.data("text", text);
+    }
 }
